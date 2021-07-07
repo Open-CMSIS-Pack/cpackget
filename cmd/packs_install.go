01da89485104e6db40d0bd4a015e4db92a88b7b6
Charles Oliveira <charles.oliveira@linaro.org>
2021-07-07 13:59:06 -0300

    packs_install: document installation rules
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- cmd/packs_install.go
+++ cmd/packs_install.go
@@ -7,6 +7,19 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Install receives a pack path (*.pack/*.zip or *pdsc) and installs packs properly
+//
+// - if packPath is a pdsc file:
+//   1. Add a pdsc tag to ".Local/local_repository.pidx"
+//     1.1. Vendor, Name and Version should be extracted from reading the pdsc file
+//     1.2. URL should be the `dirname` of the given pdsc file
+//
+// - if packPath is a pack file (either *.pdsc or *.zip extension)
+//   1. Save a copy of the file in ".Download/"
+//   2. Save a versioned pdsc file in ".Download/"
+//   3. Extract all files to "Vendor/Name/Version"
+//   4. If pack does not exist in ".Web/index.pidx"
+//     4.1. Save an unversioned copy of the pdsc file in ".Local/"
 func (manager *PacksManagerType) Install(packPath string) error {
 	log.Infof("Installing %s", packPath)
 
