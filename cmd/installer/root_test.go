f69af2892e143dd5c93da9a015ce494e11f537be
Charles Oliveira <charles.oliveira@linaro.org>
2021-08-20 15:59:48 -0300

    installer: prepare for adding integration tests
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

---
+++ cmd/installer/root_test.go
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright Contributors to the vidx2pidx project. */
+
+package installer_test
+
+import (
+	"testing"
+)
+
+// type testCase struct {
+// 	name  string
+// 	path  string
+// 	purge bool
+// 	err   error
+// 	check func() bool
+// }
+
+func TestAddPack(t *testing.T) {
+	// var tests = []testCase {
+	// 	testCase {
+	// 		name: "test fail if path is invalid",
+	// 		path: "not-really-valid",
+
+	// 	},
+	// }
+}
+
+// Add tests to the following
+// - .pack
+//   - locally
+//     - is public
+//     - is no public
+//   - from remote server
+//     - is public
+//     - is not public
+// - .zip
+//   - locally
+//     - is public
+//     - is no public
+//   - from remote server
+//     - is public
+//     - is not public
+
+// Remove packs with the following
+// - Vendor.Pack
+//   - existing pack installation
+//   - non existing pack installation
+// - Vendor.Pack.x.y.z
+//   - existing pack installation
+//   - non existing pack installation
