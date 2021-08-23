659c90b9a6f24fab0b9c5c90c356b8c76a867903
Charles Oliveira <charles.oliveira@linaro.org>
2021-08-23 08:59:22 -0300

    installer: root: added initial integration tests
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- cmd/installer/root_test.go
+++ cmd/installer/root_test.go
@@ -4,25 +4,68 @@
 package installer_test
 
 import (
+	"fmt"
 	"testing"
+
+	errs "github.com/open-cmsis-pack/cpackget/cmd/errors"
+	"github.com/open-cmsis-pack/cpackget/cmd/installer"
+	"github.com/stretchr/testify/assert"
 )
 
-// type testCase struct {
-// 	name  string
-// 	path  string
-// 	purge bool
-// 	err   error
-// 	check func() bool
-// }
+type testCase struct {
+	name  string
+	path  string
+	purge bool
+	err   error
+	check func(testCase) bool
+}
+
+func runTests(t *testing.T, tests []testCase, testFunction func(testCase) error) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := testFunction(test)
+			fmt.Println(err.Error())
+			if test.err != nil {
+				assert.True(t, errs.Is(err, test.err))
+			} else {
+				assert.True(t, test.check(test))
+			}
+		})
+	}
+}
 
 func TestAddPack(t *testing.T) {
-	// var tests = []testCase {
-	// 	testCase {
-	// 		name: "test fail if path is invalid",
-	// 		path: "not-really-valid",
+	var tests = []testCase{
+		testCase{
+			name: "test fail if path is invalid",
+			path: "not-really-valid",
+			err:  errs.ErrBadPackNameInvalidExtension,
+		},
+	}
 
-	// 	},
-	// }
+	runTests(t, tests, func(test testCase) error {
+		return installer.AddPdsc(test.path)
+	})
+}
+
+func TestRemovePack(t *testing.T) {
+	var tests = []testCase{
+		testCase{
+			name: "test fail if path is invalid",
+			path: "not-really-valid",
+			err:  errs.ErrBadPackNameInvalidExtension,
+		},
+		testCase{
+			name:  "test fail if path is invalid with purge",
+			path:  "not-really-valid",
+			purge: true,
+			err:   errs.ErrBadPackNameInvalidExtension,
+		},
+	}
+
+	runTests(t, tests, func(test testCase) error {
+		return installer.AddPdsc(test.path)
+	})
 }
 
 // Add tests to the following
